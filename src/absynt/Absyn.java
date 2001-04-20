ce255ca0af5b518291e0297d294c897aff734706
swprakt <msteffen@ifi.uio.no>
2001-04-20 05:36:15 +0000

    OK

--- src/absynt/Absyn.java
+++ src/absynt/Absyn.java
@@ -5,17 +5,13 @@ import java.io.Serializable;
 
 /**
  * Abstract class to provide coordinates and locations.
- * Um sp"ater das Parsen zu erleichten, ist ein Feld
- * vorgesehen, welches zumindest die Zeilennummber aufnehmen kann.
- * Alle syntaktischen konstrukte, bei denen Zeileninformation
- * sinnvoll ist, sollen hiervon erben.
- *
+
  * The classes of this package
- * are a straighforward implmementation of the EBNF definition,
+ * are a straighforward implemementation of the EBNF definition,
  * so in most cases, the code is self-explanatory.
  * 
  * @author Initially provided by Martin Steffen.
- * @version  $Id: Absyn.java,v 1.1 2001-04-19 12:21:13 swprakt Exp $
+ * @version  $Id: Absyn.java,v 1.2 2001-04-20 05:36:14 swprakt Exp $
  */
 
 
@@ -32,7 +28,10 @@ abstract public class Absyn implements Serializable {
 //	Abstract Syntax for Mist programs
 //	------------------------------------
 //
-//	$Id: Absyn.java,v 1.1 2001-04-19 12:21:13 swprakt Exp $
+//	$Id: Absyn.java,v 1.2 2001-04-20 05:36:14 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.1  2001/04/19 12:21:13  swprakt
+//	*** empty log message ***
+//	
 //---------------------------------------------------------------------
