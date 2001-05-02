67128758c98bcb2173a11c931e6a1e19406a9f7f
swprakt <msteffen@ifi.uio.no>
2001-05-02 05:27:07 +0000

    First proposal according to the requirement spec.
    [Steffen]

---
+++ src/absynt/Stmt.java
@@ -0,0 +1,31 @@
+
+package absynt;
+import java.io.Serializable;
+
+
+/**
+ * Statements for SFC's as part of (very) simple assignement language.
+ * The statements are implemented as abstract class, with
+ * the concrete classes as subclasses.
+ * 
+ * @author Initially provided by Martin Steffen.
+ * @version $Id: Stmt.java,v 1.1 2001-05-02 05:27:07 swprakt Exp $
+ */
+
+
+public abstract class Stmt extends Absynt implements Serializable { 
+}
+
+
+
+
+//----------------------------------------------------------------------
+//	Abstract syntax for Snot programs
+//	------------------------------------
+//
+//	$Id: Stmt.java,v 1.1 2001-05-02 05:27:07 swprakt Exp $
+//
+//	$Log: not supported by cvs2svn $
+//	
+//---------------------------------------------------------------------
+
