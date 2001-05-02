c2fa09cc0860c18bfa9fd2d31bdcb5ba90d41b66
swprakt <msteffen@ifi.uio.no>
2001-05-02 06:47:47 +0000

    *** empty log message ***

---
+++ src/absynt/StmtList.java
@@ -0,0 +1,46 @@
+package absynt;
+import java.io.Serializable;
+
+
+/**
+ * A list implementation of a set of statements.
+ * 
+ * To iterate through the list, the statements adhere to the
+ * ``Enumeration''-interface
+ * @author Initially provided by Martin Steffen.
+ * @version $Id: StmtList.java,v 1.1 2001-05-02 06:47:47 swprakt Exp $
+ */
+
+
+public class StmtList 
+  extends Absynt implements java.util.Enumeration, Serializable { 
+  public Stmt      head;
+  public StmtList  next;
+
+  public StmtList (Stmt s, StmtList sl) {
+    head = s;
+    tail = sl;
+  };
+
+  public boolean hasMoreElements (){ // required by ``Enumeration''
+    return next != null;
+  }
+  
+  public Object nextElement () { // required byte ``Enumeration''
+    // to iterate through the list.
+    return next;
+  }
+}
+
+
+
+
+//----------------------------------------------------------------------
+//	Abstract syntax for Snot programs
+//	------------------------------------
+//
+//	$Id: StmtList.java,v 1.1 2001-05-02 06:47:47 swprakt Exp $
+//
+//	$Log: not supported by cvs2svn $
+//---------------------------------------------------------------------
+
