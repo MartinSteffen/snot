b1b09bafc5803b89d32389dec9d0187431e84e62
swprakt <msteffen@ifi.uio.no>
2001-05-02 05:52:04 +0000

    I added a field for types and a second consctructor.
    [Steffen]

--- src/absynt/Variable.java
+++ src/absynt/Variable.java
@@ -7,15 +7,22 @@ import java.io.Serializable;
  * They are very simply implemented as strings
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Variable.java,v 1.1 2001-05-02 05:47:32 swprakt Exp $	
+ * @version $Id: Variable.java,v 1.2 2001-05-02 05:52:04 swprakt Exp $	
  */
 
 
 public class Variable extends Expr implements Serializable { 
   public String name;
+  public Type   type;
 
   public Variable (String s) {
-    name = s;};
+    name = s;
+    type = nil;
+  };
+
+  public Variable (String s, Type _t) {
+    name = s;
+    type  = _t};
 };
 
 
@@ -25,9 +32,14 @@ public class Variable extends Expr implements Serializable {
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: Variable.java,v 1.1 2001-05-02 05:47:32 swprakt Exp $
+//	$Id: Variable.java,v 1.2 2001-05-02 05:52:04 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.1  2001/05/02 05:47:32  swprakt
+//	First proposal according to the req. spec.
+//	
+//	[Steffen]
+//	
 //	
 //---------------------------------------------------------------------
 
