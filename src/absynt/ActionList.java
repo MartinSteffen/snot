b98fe9ae8b694642eb6e149bf2b6ad8007f361d3
swprakt <msteffen@ifi.uio.no>
2001-05-02 06:36:47 +0000

    First proposal according to the requirement specification.
    [Steffen]

---
+++ src/absynt/ActionList.java
@@ -0,0 +1,47 @@
+package absynt;
+import java.io.Serializable;
+
+
+/**
+ * A list implementation of a set of actions.
+ * 
+ * To iterate through the list, the steps adhere to the
+ * ``Enumaration''-interface
+ * @author Initially provided by Martin Steffen.
+ * @version $Id: ActionList.java,v 1.1 2001-05-02 06:36:47 swprakt Exp $
+ */
+
+
+public class ActionList 
+  extends Absynt implements java.util.Enumeration, Serializable { 
+  public Action      head;
+  public ActionList  next;
+
+  public ActionList (Action s, ActionList sl) {
+    head = s;
+    tail = sl;
+  };
+
+  public boolean hasMoreElements (){ // required by ``Enumeration''
+    return next != null;
+  }
+  
+  public Object nextElement () { // required byte ``Enumeration''
+    // to iterate through the list.
+    return next;
+  }
+}
+
+
+
+
+//----------------------------------------------------------------------
+//	Abstract syntax for Snot programs
+//	------------------------------------
+//
+//	$Id: ActionList.java,v 1.1 2001-05-02 06:36:47 swprakt Exp $
+//
+//	$Log: not supported by cvs2svn $
+//	
+//---------------------------------------------------------------------
+
