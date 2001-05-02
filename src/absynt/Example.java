d79c9f7cac02aac0b940d83cdf711a9378f44ded
swprakt <msteffen@ifi.uio.no>
2001-05-02 07:37:02 +0000

    Example still empty

--- src/absynt/Example.java
+++ src/absynt/Example.java
@@ -5,7 +5,7 @@ package absynt;
  * 
  * The class offers an example for a program int abstract syntax.
  * @author Initially provided by Martin Steffen.
- * @version $Id: Example.java,v 1.4 2001-05-02 07:24:51 swprakt Exp $	
+ * @version $Id: Example.java,v 1.5 2001-05-02 07:37:02 swprakt Exp $	
  */
 
 
@@ -19,6 +19,12 @@ public class Example{
   /*
    * For enhanced readability, the example is built stp-byte-step.
    */
+      Variable v_x = new Variable ("x");   
+      Constval c5  = new Constval (5);
+      Assign stmt1 = new Assign(v_x, c5);  // x := 5
+      Assign stmp2 = 
+	new Assign(v_x,
+		   new U_expr(Expr.NEG, v_x));
       return null;
     };
 }
@@ -30,9 +36,12 @@ public class Example{
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: Example.java,v 1.4 2001-05-02 07:24:51 swprakt Exp $
+//	$Id: Example.java,v 1.5 2001-05-02 07:37:02 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.4  2001/05/02 07:24:51  swprakt
+//	Example compiles (but so far is empty)
+//	
 //	
 //---------------------------------------------------------------------
 
