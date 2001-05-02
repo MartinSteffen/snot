b606715b6d10276dc8c998518047524afcdf0b47
swprakt <msteffen@ifi.uio.no>
2001-05-02 05:36:10 +0000

    First proposal according to the requirement spec. [Steffen]

---
+++ src/absynt/Skip.java
@@ -0,0 +1,29 @@
+package absynt;
+import java.io.Serializable;  // for instance 
+
+
+/**
+ * A skip statement as part of the simple assignement language
+ * @author Initially provided by Martin Steffen
+ * @version $Id: Skip.java,v 1.1 2001-05-02 05:36:10 swprakt Exp $
+ */
+
+
+public class Skip extends Stmt implements Serializable { 
+  // -------------
+  public Skip () {};    // in principle, covered byte default constructor
+}
+
+
+
+
+//----------------------------------------------------------------------
+//	Abstract syntax for Snot programs
+//	-----------------------------------
+//
+//	$Id: Skip.java,v 1.1 2001-05-02 05:36:10 swprakt Exp $
+//
+//	$Log: not supported by cvs2svn $
+//	
+//---------------------------------------------------------------------
+
