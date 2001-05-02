4b20f04cd8961e1b69efd3a5ef97cebfb8aad8e4
swprakt <msteffen@ifi.uio.no>
2001-05-02 06:41:36 +0000

    I renamed the class, not Abyn but Absynt, as the name of the package.
    [Steffen]

---
+++ src/absynt/Absynt.java
@@ -0,0 +1,33 @@
+package absynt;
+import java.io.Serializable;
+
+/**
+ * Abstract class to provide coordinates and locations.
+
+ * The classes of this package
+ * are a straighforward implemementation of the EBNF definition,
+ * so in most cases, the code is self-explanatory.
+ * 
+ * @author Initially provided by Martin Steffen.
+ * @version  $Id: Absynt.java,v 1.1 2001-05-02 06:41:36 swprakt Exp $
+ */
+
+
+
+abstract public class Absynt implements Serializable {
+    public Location location;
+};
+
+
+
+
+
+//----------------------------------------------------------------------
+//	Abstract syntax for Snot programs
+//	------------------------------------
+//
+//	$Id: Absynt.java,v 1.1 2001-05-02 06:41:36 swprakt Exp $
+//
+//	$Log: not supported by cvs2svn $
+
+//---------------------------------------------------------------------
