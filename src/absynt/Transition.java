52f71d08902d8903e5cee8b40036d0f2d2cbac83
swprakt <msteffen@ifi.uio.no>
2001-05-03 14:02:08 +0000

    Variablen sind jetzt public

--- src/absynt/Transition.java
+++ src/absynt/Transition.java
@@ -6,14 +6,14 @@ import java.io.Serializable;
  * A transition of an SFC connects source and target step
  * and is labelled with a guard.
  * @author Initially provided by Martin Steffen.
- * @version $Id: Transition.java,v 1.2 2001-05-02 07:03:37 swprakt Exp $	
+ * @version $Id: Transition.java,v 1.3 2001-05-03 14:02:08 swprakt Exp $	
  */
 
 
 public class Transition extends Absynt implements Serializable { 
-  StepList source;
-  Expr     guard;
-  StepList target;
+  public StepList source;
+  public Expr     guard;
+  public StepList target;
   //------------------
   public Transition (StepList _s, Expr _g, StepList _t) {
     source = _s;
@@ -29,9 +29,12 @@ public class Transition extends Absynt implements Serializable {
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: Transition.java,v 1.2 2001-05-02 07:03:37 swprakt Exp $
+//	$Id: Transition.java,v 1.3 2001-05-03 14:02:08 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2001/05/02 07:03:37  swprakt
+//	Abstract syntax compiles.
+//	
 //	Revision 1.1  2001/05/02 05:56:06  swprakt
 //	First proposal according to the requirement spec.
 //	
