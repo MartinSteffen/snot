16674bd9a2d6e89612be2808c1e7a5a421bc1309
swprakt <msteffen@ifi.uio.no>
2001-05-03 13:31:29 +0000

    Repaired an error: Assign is a subclass of Stmt

--- src/absynt/Assign.java
+++ src/absynt/Assign.java
@@ -6,11 +6,11 @@ import java.io.Serializable;
  * An assignment-statement as part of the simple assignment language
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Assign.java,v 1.2 2001-05-02 07:03:36 swprakt Exp $
+ * @version $Id: Assign.java,v 1.3 2001-05-03 13:31:29 swprakt Exp $
  */
 
 
-public class Assign extends Absynt implements Serializable { 
+public class Assign extends Stmt implements Serializable { 
   public Variable var;
   public Expr     val;
 
@@ -28,9 +28,12 @@ public class Assign extends Absynt implements Serializable {
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: Assign.java,v 1.2 2001-05-02 07:03:36 swprakt Exp $
+//	$Id: Assign.java,v 1.3 2001-05-03 13:31:29 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2001/05/02 07:03:36  swprakt
+//	Abstract syntax compiles.
+//	
 //	Revision 1.1  2001/05/02 05:39:21  swprakt
 //	First proposal
 //	
