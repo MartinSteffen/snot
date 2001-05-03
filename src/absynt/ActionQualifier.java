20ba866d852e86f5c235a31891fe4268fdc370aa
swprakt <msteffen@ifi.uio.no>
2001-05-03 13:20:45 +0000

    *** empty log message ***

--- src/absynt/ActionQualifier.java
+++ src/absynt/ActionQualifier.java
@@ -6,21 +6,12 @@ import java.io.Serializable;
  * ActionQualifier.java
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: ActionQualifier.java,v 1.2 2001-05-02 07:03:36 swprakt Exp $
+ * @version $Id: ActionQualifier.java,v 1.3 2001-05-03 13:20:45 swprakt Exp $
  */
 
 
-public class ActionQualifier  extends Absynt implements Serializable { 
-    /**
-     * ``Constants''.
-     */
-    public final static int NQUAL  = 0;
-    public final static int RQUAL  = 1;
-    public final static int SQUAL  = 2;
-    public final static int P1QUAL = 3;
-    public final static int P2QUAL = 4;
+public abstract class ActionQualifier  extends Absynt implements Serializable { 
 }
-
 
 
 
@@ -28,9 +19,12 @@ public class ActionQualifier  extends Absynt implements Serializable {
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: ActionQualifier.java,v 1.2 2001-05-02 07:03:36 swprakt Exp $
+//	$Id: ActionQualifier.java,v 1.3 2001-05-03 13:20:45 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2001/05/02 07:03:36  swprakt
+//	Abstract syntax compiles.
+//	
 //	Revision 1.1  2001/04/20 17:29:11  swprakt
 //	ok
 //	
---
+++ src/absynt/Nqual.java
@@ -0,0 +1,26 @@
+package absynt;
+import java.io.Serializable;
+
+
+/**
+ * @author Initially provided by Martin Steffen.
+ * @version $Id: Nqual.java,v 1.1 2001-05-03 13:17:43 swprakt Exp $
+ */
+
+
+public class Nqual extends ActionQualifier implements Serializable { 
+}
+
+
+
+
+//----------------------------------------------------------------------
+//	Abstract syntax for Snot programs
+//	------------------------------------
+//
+//	$Id: Nqual.java,v 1.1 2001-05-03 13:17:43 swprakt Exp $
+//
+//	$Log: not supported by cvs2svn $
+//	
+//---------------------------------------------------------------------
+
