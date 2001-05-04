af18508fdcc6ddb4e237237dd8ee13fe9e06616d
swprakt <msteffen@ifi.uio.no>
2001-05-04 09:27:29 +0000

    removed bug, changed field from `a_name' to `name'

--- src/absynt/Step.java
+++ src/absynt/Step.java
@@ -6,17 +6,17 @@ import java.io.Serializable;
  * A step of a Snot-program.
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Step.java,v 1.2 2001-05-02 07:03:37 swprakt Exp $
+ * @version $Id: Step.java,v 1.3 2001-05-04 09:27:29 swprakt Exp $
  */
 
 
 public class Step extends Absynt implements Serializable { 
-  public String a_name;
+  public String name;
   public ActionList actions;
   //-------------------------------
-  public Step (String _a_name, ActionList _actions) {
-    a_name = _a_name;
-    _actions = actions;
+  public Step (String _name, ActionList _actions) {
+    name = _name;
+    actions = _actions;
   }
 }
 
@@ -27,9 +27,12 @@ public class Step extends Absynt implements Serializable {
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: Step.java,v 1.2 2001-05-02 07:03:37 swprakt Exp $
+//	$Id: Step.java,v 1.3 2001-05-04 09:27:29 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2001/05/02 07:03:37  swprakt
+//	Abstract syntax compiles.
+//	
 //	Revision 1.1  2001/04/27 08:52:38  swprakt
 //	OK
 //	
