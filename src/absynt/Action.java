437568b7d4cee4e830138f87895ecab86eb44e4e
swprakt <msteffen@ifi.uio.no>
2001-05-04 09:50:38 +0000

    *** empty log message ***

--- src/absynt/Action.java
+++ src/absynt/Action.java
@@ -7,17 +7,17 @@ import java.io.Serializable;
  * and a program to be executed.
  *
  * @author Initially provided by Martin Steffen.
- * @version $Id: Action.java,v 1.2 2001-05-02 05:11:25 swprakt Exp $
+ * @version $Id: Action.java,v 1.3 2001-05-04 09:50:38 swprakt Exp $
  */
 
 
 public class Action  extends Absynt implements Serializable { 
-  public ActionQualifier qualifier;
-  public StmtList        sap;    // simple assignement program 
+  public String   a_name;
+  public StmtList sap;    // simple assignement program 
 
-  public Action (ActionQualifier _qualifier, StmtList _sap) {
-    qualifier = _qualifier;
-    sap       = _sap;
+  public Action (String _a_name, StmtList _sap) {
+    a_name = _a_name;
+    sap    = _sap;
   }
   
 }
@@ -29,9 +29,12 @@ public class Action  extends Absynt implements Serializable {
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: Action.java,v 1.2 2001-05-02 05:11:25 swprakt Exp $
+//	$Id: Action.java,v 1.3 2001-05-04 09:50:38 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2001/05/02 05:11:25  swprakt
+//	I added the standard constructor. [Steffen]
+//	
 //	Revision 1.1  2001/05/02 05:09:34  swprakt
 //	First proposal according to the req. spec.
 //	
