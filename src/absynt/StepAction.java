c20282ff54e4b4072f473093db83bc638511418b
swprakt <msteffen@ifi.uio.no>
2001-05-05 10:14:22 +0000

    Requirement spec. adapted to the implementation
    [Steffen]

--- src/absynt/StepAction.java
+++ src/absynt/StepAction.java
@@ -7,19 +7,18 @@ import java.io.Serializable;
  * and the name of  program to be executed.
  *
  * @author Initially provided by Martin Steffen.
- * @version $Id: StepAction.java,v 1.2 2001-05-05 09:45:56 swprakt Exp $
+ * @version $Id: StepAction.java,v 1.3 2001-05-05 10:14:21 swprakt Exp $
  */
 
 
 public class StepAction  extends Absynt implements Serializable { 
   public ActionQualifier qualifier;
-  public String          a_name;    // simple assignement program 
+  public String          a_name;    // name of program 
 
   public StepAction (ActionQualifier _qualifier, String _a_name) {
     qualifier = _qualifier;
     a_name    = _a_name;
   }
-  
 }
 
 
@@ -29,9 +28,12 @@ public class StepAction  extends Absynt implements Serializable {
 //	Abstract syntax for Snot programs
 //	------------------------------------
 //
-//	$Id: StepAction.java,v 1.2 2001-05-05 09:45:56 swprakt Exp $
+//	$Id: StepAction.java,v 1.3 2001-05-05 10:14:21 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2001/05/05 09:45:56  swprakt
+//	I adapted the comment [Steffen]
+//	
 //	Revision 1.1  2001/05/04 15:10:19  swprakt
 //	changed actions in steps
 //	
