608768f6661ed4966f150bade07a6c5a7247aee7
swprakt <msteffen@ifi.uio.no>
2001-05-22 14:15:42 +0000

    erster versuch

---
+++ src/checks/Snotcheck.java
@@ -0,0 +1,18 @@
+package checks;
+
+import java.lang.*;
+
+
+/**
+ *Diese Klasse macht bla bla!
+ *@author Dimitri Schultheis, Tobias Pugatschov
+ *@$version: 0.0.1 Beta$
+ *
+ */
+
+
+public class Snotcheck{
+
+
+
+}
