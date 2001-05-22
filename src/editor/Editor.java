bd708b01443cddf416e804345aba955efc2283a0
swprakt <msteffen@ifi.uio.no>
2001-05-22 14:56:14 +0000

    kleinste Änderungen

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -1,7 +1,9 @@
 package editor;
 
+import java.util.Enumeration;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.*;
 import javax.swing.*;
 import absynt.*;
 
@@ -16,16 +18,69 @@ public class Editor extends JFrame {
 
   private class ActInsertStep extends AbstractAction {
     public ActInsertStep() {
-      super("InsertStep", new ImageIcon("Step.bmp"));
+      super("InsertStep"); //, new ImageIcon("Step.bmp"));
     }
 
     public void actionPerformed(ActionEvent e) {
+      repaint();
+    }
+  }
+
+  private class DrawSFCPanel extends JPanel {
+
+    private void paintTrans(Transition Trans, Graphics2D G2D) {
+      Step step;
+      StepList SL = Trans.source;
+      float minX = 10000.0f, maxX = 0.0f;
+
+      while (SL != null && SL.head != null) {
+        step = (Step)SL.head;
+        G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f));
+        G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f);
+        if (step.pos.x < minX) minX=step.pos.x;
+        if (step.pos.x > maxX) maxX=step.pos.x;
+        SL = (StepList)SL.nextElement();
+      }
+
+      SL = Trans.target;
+      while (SL != null && SL.head != null) {
+        step = (Step)SL.head;
+        G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f));
+        G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f);
+        SL = (StepList)SL.nextElement();
+      }
+    }
+
+    private void paintSFC(Graphics2D G2D) {
+      Transition Trans;
+      TransitionList TS = sfc.transs;
+
+      while (TS != null && TS.head != null) {
+        Trans = (Transition)TS.head;
+        paintTrans(Trans, G2D);
+        TS = (TransitionList)TS.nextElement();
+      }
+
+    }
+
+    public void paint(Graphics g) {
+      Graphics2D G2D = (Graphics2D)g;
+      //GeneralPath Path = new GeneralPath();
+      //Font font = new Font("
+
+      super.paint(g);
+      //G2D.scale(10.0f, 10.0f);
+      G2D.setColor(Color.black);
+      if (sfc != null) paintSFC(G2D);
+      //G2D.draw(Path);
     }
   }
 
   private JToolBar ToolBar;
-  private JScrollPane ScrollPane;
-  private JPanel DrawPanel;
+  private JScrollPane DrawScrPane;
+  private DrawSFCPanel DrawPanel;
+  private JScrollPane DataScrPane;
+  private JPanel DataPanel;
 
   private String strName;
   private boolean boolModified;
@@ -45,7 +100,7 @@ public class Editor extends JFrame {
 
   /**
    * setzt den Dateinamen
-   * (nur zur Anzeige in der Titelleise, Datei-IO macht ansonsten die GUI-Gruppe
+   * (nur zur Anzeige in der Titelleise, Datei-IO macht ansonsten die GUI-Gruppe)
    */
   private void setFilename(String Filename) {
     strName = new String(Filename);
@@ -58,8 +113,53 @@ public class Editor extends JFrame {
     // hier noch neuzeichen
   }
 
-  public void repaintSFC() {
+  private void proccessTrans(Transition Trans, int PosY) {
+    Step step;
+    StepList SL = Trans.source;
+    int numStep = 0;
 
+   System.out.println("  " + "SourceSteps:");
+    while (SL != null && SL.head != null) {
+      step = (Step)SL.head;
+      System.out.print("    " + "Step " + step.name + " : PosY=" + PosY + " - ");
+      if (step.pos == null || step.pos.x == -1)
+        step.pos = new Position((float)numStep*2f, (float)PosY);
+      numStep+=1;
+      SL = (StepList)SL.nextElement();
+    }
+    System.out.println("");
+
+    SL = Trans.target;  PosY+=1;  numStep=0;
+    System.out.println("  " + "TargetSteps:");
+    while (SL != null && SL.head != null) {
+      step = (Step)SL.head;
+      System.out.print("    " + "Step " + step.name + " : PosY=" + PosY + " - ");
+      if (step.pos == null || step.pos.x == -1)
+        step.pos = new Position(numStep, PosY);
+      numStep+=1;
+      SL = (StepList)SL.nextElement();
+    }
+    System.out.println("");
+  }
+
+  public void realignSFC() {
+    Transition Trans;
+    TransitionList TS = sfc.transs;
+    int PosY = 0, TransNum = 1;
+
+    while (TS != null && TS.head != null) {
+      Trans = (Transition)TS.head;
+      System.out.println("Transition-Nr.: " + TransNum);
+      proccessTrans(Trans, PosY);
+      PosY+=3;  TransNum+=1;
+      TS = (TransitionList)TS.nextElement();
+    }
+
+  }
+
+  public void repaintSFC() {
+    realignSFC();
+    repaint();
   }
 
   /**
@@ -68,38 +168,36 @@ public class Editor extends JFrame {
    */
   public Editor(SFC anSFC) {
     setFilename("NoName");
-    setSize(300, 320);
+    setSize(400, 420);
     setSFC(anSFC);
+
+    // ToolBar:
     ToolBar = new JToolBar();
-    ToolBar.add(new ActInsertStep());  ToolBar.setSize(300, 40);
+    ToolBar.add(new ActInsertStep());  ToolBar.setSize(400, 80);
     getContentPane().add(ToolBar, BorderLayout.NORTH);
 
-    DrawPanel = new JPanel();
+    // Panel für Variablen- / Aktionen-Deklaration:
+    DataPanel = new JPanel();
+    DataPanel.setSize(200, 0);
+    DataPanel.setLayout(new GridLayout(0, 1));
+    DataPanel.add(new JLabel("Deklarationen:"));
+    DataPanel.add(new JLabel("Aktionen:"));
+    // DataPanel.add(new JTable, BorderLayout.NORTH);
+    DataScrPane = new JScrollPane(DataPanel);
+    DataScrPane.setSize(200, 0);
+    getContentPane().add(DataScrPane, BorderLayout.WEST);
+
+
+    // Zeichenfläche für SFC:
+    DrawPanel = new DrawSFCPanel();
     DrawPanel.setBackground(Color.white);
 
-    ScrollPane = new JScrollPane(DrawPanel);
-    getContentPane().add(ScrollPane, BorderLayout.CENTER);
+    DrawScrPane = new JScrollPane(DrawPanel);
+    getContentPane().add(DrawScrPane, BorderLayout.CENTER);
 
     repaintSFC();
 
   }
-
-  /* saveToFile und loadFromFile fliegen raus, da Gui-Gruppe sich darum
-     kümmert:
-
-  public boolean saveToFile(String Filename) {
-    System.out.println("Speichern unter: " + Filename);
-    setFilename(Filename);
-    return(true);
-  }
-
-  public boolean loadFromFile(String Filename) {
-    System.out.println("Lade: " + Filename);
-    setFilename(Filename);
-    return(true);
-  }
-
-  */
 
   /**
    * setzt den Highlight-Status eines Absynt-Elements -
