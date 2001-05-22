e2ca2fe6733fd4faf59f3e0d9f1401ea37333fa4
swprakt <msteffen@ifi.uio.no>
2001-05-22 14:24:28 +0000

    *** empty log message ***

--- src/checks/Snotcheck.java
+++ src/checks/Snotcheck.java
@@ -5,8 +5,9 @@ import java.lang.*;
 
 /**
  *Diese Klasse macht bla bla!
+ *
  *@author Dimitri Schultheis, Tobias Pugatschov
- *@version: $Id: Snotcheck.java,v 1.2 2001-05-22 14:16:08 swprakt Exp $
+ *@version: $Id: Snotcheck.java,v 1.3 2001-05-22 14:24:28 swprakt Exp $
  *
  */
 
@@ -14,5 +15,13 @@ import java.lang.*;
 public class Snotcheck{
 
 
+
+    public boolean isWellDefine(SFC aSFCObject){
+	return true;
+    }
+
+    public boolean onlyBool(SFC aSFCObject){
+	return false;
+    }
 
 }
