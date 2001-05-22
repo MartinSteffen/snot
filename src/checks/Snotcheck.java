0dbd69bdc9f890f331d9344c49e2110990207685
swprakt <msteffen@ifi.uio.no>
2001-05-22 14:28:04 +0000

    *** empty log message ***

--- src/checks/Snotcheck.java
+++ src/checks/Snotcheck.java
@@ -4,10 +4,12 @@ import java.lang.*;
 
 
 /**
- *Diese Klasse macht bla bla!
- *
+ *Diese Klasse macht eine ganze Menge, n"amlich zum Beispiel:
+ *-checken von SFCs
+ *-checken von SFCs
+ *-und nat"urlich auch checken von SFCs
  *@author Dimitri Schultheis, Tobias Pugatschov
- *@version: $Id: Snotcheck.java,v 1.4 2001-05-22 14:25:23 swprakt Exp $
+ *@version: $Id: Snotcheck.java,v 1.5 2001-05-22 14:28:04 swprakt Exp $
  *
  */
 
@@ -31,8 +33,11 @@ public class Snotcheck{
 //	package checks for Snot programs
 //	------------------------------------
 //
-//	$Id: Snotcheck.java,v 1.4 2001-05-22 14:25:23 swprakt Exp $
+//	$Id: Snotcheck.java,v 1.5 2001-05-22 14:28:04 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.4  2001/05/22 14:25:23  swprakt
+//	ok
+//	
 //	
 //---------------------------------------------------------------------
