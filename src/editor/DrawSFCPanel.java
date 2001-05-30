1a664276c5e9cd212d81b40534bd9a5757d6fdb5
swprakt <msteffen@ifi.uio.no>
2001-05-30 16:00:44 +0000

    erste Erweiterungen zum Zeichnen

--- src/editor/DrawSFCPanel.java
+++ src/editor/DrawSFCPanel.java
@@ -1,6 +1,6 @@
 package editor;
 
-import java.util.Enumeration;
+import java.util.LinkedList;
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.font.*;
@@ -8,73 +8,81 @@ import javax.swing.*;
 import absynt.*;
 import editor.StepPosition;
 
-public class DrawSFCPanel extends JPanel {
+ public class DrawSFCPanel extends JPanel { 
 
-  private Font sfcFont;
-  private float zoomFac = 1.0f;
-  private SFC sfc;
+     private SFC sfc;
 
-  private void paintTrans(Transition Trans, Graphics2D G2D) {
-    Step step;
-    StepList SL = Trans.source;
-    float minX = 10000.0f, maxX = 0.0f;
+     public int editorAction;
 
-    while (SL != null && SL.head != null) {
-      step = (Step)SL.head;
-      G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f));
-      G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f);
-      if (step.pos.x < minX) minX=step.pos.x;
-      if (step.pos.x > maxX) maxX=step.pos.x;
-      SL = (StepList)SL.nextElement();
-    }
+     final static public int SELECT = 0;
+     final static public int INSERT_STEP = 1;
+     final static public int INSERT_TRANS = 2;
+     final static public int DELETE = 3;
 
-    SL = Trans.target;
-    while (SL != null && SL.head != null) {
-      step = (Step)SL.head;
-      G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f));
-      G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f);
-      SL = (StepList)SL.nextElement();
-    }
-  }
+     private void paintTrans(Transition Trans, Graphics2D G2D) { 
+      Step step; 
+      LinkedList stepLL = Trans.source; 
+      float minX = 10000.0f, maxX = 0.0f; 
+      int i; 
+ 
+      if (stepLL != null) { 
+        for (i=0; i < stepLL.size(); i++) { 
+          step = (Step)stepLL.get(i); 
+          G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f)); 
+          G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f); 
+          if (step.pos.x < minX) minX=step.pos.x; 
+          if (step.pos.x > maxX) maxX=step.pos.x; 
+        } 
+      } 
+ 
+      stepLL = Trans.target; 
+      if (stepLL != null) { 
+        for (i=0; i < stepLL.size(); i++) { 
+          step = (Step)stepLL.get(i); 
+          G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f)); 
+          G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f); 
+        } 
+      } 
+    } 
+ 
+    private void paintSFC(Graphics2D G2D) { 
+      Transition Trans; 
+      LinkedList transLL = sfc.transs; 
+ 
+      if (transLL != null) { 
+        for (int i=0; i < transLL.size(); i++) { 
+          Trans = (Transition)transLL.get(i); 
+          paintTrans(Trans, G2D); 
+        } 
+      } 
+ 
+    } 
+ 
+    public void paint(Graphics g) { 
+      Graphics2D G2D = (Graphics2D)g; 
+      //GeneralPath Path = new GeneralPath(); 
+      //Font font = new Font(" 
+ 
+      super.paint(g); 
+      //G2D.scale(10.0f, 10.0f); 
+      G2D.setColor(Color.black); 
+      if (sfc != null) paintSFC(G2D); 
+      //G2D.draw(Path); 
+    } 
 
-  private void paintSFC(Graphics2D G2D) {
-    Transition Trans;
-    TransitionList TS = sfc.transs;
+    public DrawSFCPanel(SFC anSFC) {
+      sfc = anSFC;
+      enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+    } 
 
-    while (TS != null && TS.head != null) {
-      Trans = (Transition)TS.head;
-      paintTrans(Trans, G2D);
-      TS = (TransitionList)TS.nextElement();
-    }
-
-  }
-
-  public void paint(Graphics g) {
-    Graphics2D G2D = (Graphics2D)g;
-    if (sfcFont == null) sfcFont = new Font("Helvetica", Font.PLAIN, 10);
-    //GeneralPath Path = new GeneralPath();
-    //Font font = new Font("
-
-    super.paint(g);
-    //G2D.scale(10.0f, 10.0f);
-    G2D.setColor(Color.black);
-    if (sfc != null) paintSFC(G2D);
-    //G2D.draw(Path);
-  }
-
-  public void setSFCFont(Font _font) {
-    sfcFont = _font.deriveFont(_font.getSize2D());
-    repaint();
-  }
-
-  public Font getSFCFont() {
-    return (sfcFont.deriveFont(sfcFont.getSize2D()));
-  }
-
-  public DrawSFCPanel(SFC _sfc) {
-    super();
-    sfcFont = new Font("Helvetica", Font.PLAIN, 10);
-    sfc = _sfc;
-  }
-
-}
+    protected void processMouseEvent(MouseEvent e) {
+	//super.processMouseEvent(e);
+      if (e.getID() == MouseEvent.MOUSE_PRESSED) {
+        Step step = new Step("Neuer Step");
+        step.Pos = new StepPosition(e.getX().doubleValue()/30, e.getY().doubleValue()/30, 1f, 1f);
+        sfc.steps.add(step);
+	repaint();
+	System.out.println("Mouse");
+      }
+    } 
+  } 
--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -1,292 +1,281 @@
-package editor;
+package editor; 
+ 
+import java.util.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+import java.awt.geom.*; 
+import javax.swing.*; 
+import absynt.*; 
+import editor.DrawSFCPanel;
+ 
+/** 
+ * Editor - Klasse 
+ * 
+ * @author Natalja Froidenberg, Andreas Lukosch 
+ * @version 1.0 
+ */ 
+ 
+public class Editor extends JFrame { 
 
-import java.util.LinkedList;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.*;
-import javax.swing.*;
-import absynt.*;
-
-/**
- * Editor - Klasse
- *
- * @author Natalja Froidenberg, Andreas Lukosch
- * @version 1.0
- */
-
-public class Editor extends JFrame {
-
-  private class ActInsertStep extends AbstractAction {
-    public ActInsertStep() {
-      super("InsertStep"); 
-    }
-
-    public void actionPerformed(ActionEvent e) {
-      repaint();
-    }
-  }
-  
-  private class ActInsertTrans extends AbstractAction {
-    public ActInsertTrans() {
-      super("InsertTrans"); 
-    }
-
-    public void actionPerformed(ActionEvent e) {
-      repaint();
-    }
-  }
-  
-  private class ActDeleteAbsynt extends AbstractAction {
-    public ActDeleteAbsynt() {
-      super("Delete"); 
-    }
-
-    public void actionPerformed(ActionEvent e) {
-      repaint();
-    }
-  }
-
-  private class DrawSFCPanel extends JPanel {
-
-    private void paintTrans(Transition Trans, Graphics2D G2D) {
-      Step step;
-      LinkedList stepLL = Trans.source;
-      float minX = 10000.0f, maxX = 0.0f;
-      int i;
-
-      if (stepLL != null) {
-        for (i=0; i < stepLL.size(); i++) {
-          step = (Step)stepLL.get(i);
-          G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f));
-          G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f);
-          if (step.pos.x < minX) minX=step.pos.x;
-          if (step.pos.x > maxX) maxX=step.pos.x;
-        }
-      }
-
-      stepLL = Trans.target;
-      if (stepLL != null) {
-        for (i=0; i < stepLL.size(); i++) {
-          step = (Step)stepLL.get(i);
-          G2D.draw(new Rectangle2D.Float(step.pos.x*30f, step.pos.y*30f, 30.0f, 30.0f));
-          G2D.drawString(step.name, step.pos.x*30f, (step.pos.y+1)*30f);
-        }
-      }
-    }
-
-    private void paintSFC(Graphics2D G2D) {
-      Transition Trans;
-      LinkedList transLL = sfc.transs;
-
-      if (transLL != null) {
-        for (int i=0; i < transLL.size(); i++) {
-          Trans = (Transition)transLL.get(i);
-          paintTrans(Trans, G2D);
-        }
-      }
-
-    }
-
-    public void paint(Graphics g) {
-      Graphics2D G2D = (Graphics2D)g;
-      //GeneralPath Path = new GeneralPath();
-      //Font font = new Font("
-
-      super.paint(g);
-      //G2D.scale(10.0f, 10.0f);
-      G2D.setColor(Color.black);
-      if (sfc != null) paintSFC(G2D);
-      //G2D.draw(Path);
-    }
-  }
-
-  private JToolBar ToolBar;
-  private JScrollPane DrawScrPane;
-  private DrawSFCPanel DrawPanel;
-  private JScrollPane DataScrPane;
-  private JPanel DataPanel, DataDeclPanel, DataActPanel;
+  private class ActSelect extends AbstractAction { 
+    public ActSelect() { 
+      super("Select");  
+    } 
+ 
+    public void actionPerformed(ActionEvent e) { 
+      System.out.println("Select");  
+      DrawPanel.editorAction = DrawSFCPanel.SELECT;
+    } 
+  } 
+ 
+  private class ActInsertStep extends AbstractAction { 
+    public ActInsertStep() { 
+      super("InsertStep");  
+    } 
+ 
+    public void actionPerformed(ActionEvent e) { 
+      System.out.println("InsertStep");
+      if (DrawPanel.editorAction == DrawSFCPanel.INSERT_STEP)
+        SelectToggleBtn.doClick();
+      else
+        DrawPanel.editorAction = DrawSFCPanel.INSERT_STEP;
+    } 
+  } 
+   
+  private class ActInsertTrans extends AbstractAction { 
+    public ActInsertTrans() { 
+      super("InsertTrans");  
+    } 
+ 
+    public void actionPerformed(ActionEvent e) { 
+      System.out.println("InsertTrans");
+      if (DrawPanel.editorAction == DrawSFCPanel.INSERT_TRANS)
+        SelectToggleBtn.doClick();
+      else
+        DrawPanel.editorAction = DrawSFCPanel.INSERT_TRANS;
+    } 
+  } 
+   
+  private class ActDeleteAbsynt extends AbstractAction { 
+    public ActDeleteAbsynt() { 
+      super("Delete");  
+    } 
+ 
+    public void actionPerformed(ActionEvent e) { 
+      System.out.println("Delete"); 
+      if (DrawPanel.editorAction == DrawSFCPanel.DELETE)
+        SelectToggleBtn.doClick();
+      else
+        DrawPanel.editorAction = DrawSFCPanel.DELETE;
+    } 
+  } 
+ 
+ 
+ 
+  private JToolBar ToolBar; 
+  private JScrollPane DrawScrPane; 
+  private DrawSFCPanel DrawPanel; 
+  private JScrollPane DataScrPane; 
+  private JPanel DataPanel, DataDeclPanel, DataActPanel; 
   private JTable DataDeclTable, DataActTable;
+  private JToggleButton SelectToggleBtn; 
+ 
+  private String strName; 
+  private boolean boolModified; 
+  private boolean boolChecked; 
+  private SFC sfc; 
 
-  private String strName;
-  private boolean boolModified;
-  private boolean boolChecked;
-  private SFC sfc;
+  
 
-  /**
-   * Konstante zum Highlight ausschalten
-   * @see #highlight_state(Absynt Element, boolean Value)
-   */
-  final public boolean HILIGHT_OFF = false;
-  /**
-   * Konstante zum Highlight einschalten
-   * @see #highlight_state(Absynt Element, boolean Value)
-  */
-  final public boolean HILIGHT_ON = true;
-
-  /**
-   * setzt den Dateinamen
-   * (nur zur Anzeige in der Titelleise, Datei-IO macht ansonsten die GUI-Gruppe)
-   */
-  private void setFilename(String Filename) {
-    strName = new String(Filename);
-    setTitle("Editor - " + strName);
-    boolModified = false;
-  }
-
-  private void setSFC(SFC anSFC) throws EditorException {
-    if (anSFC != null) 
-	sfc = anSFC;
-    else       
-      throw new EditorException("Anwendungsfehler: SFC = null");
-  }
-
-  private void proccessTrans(Transition Trans, int PosY) {
-    Step step;
-    LinkedList stepLL = Trans.source;
-    int numStep = 0, i = 0;
-
-    System.out.println("  " + "SourceSteps:");
-    if (stepLL != null) {
-      for (i=0; i < stepLL.size(); i++) {
-        step = (Step)stepLL.get(i);
-        System.out.print("    " + "Step " + step.name + " : PosY=" + PosY + " - ");
-        if (step.pos == null || step.pos.x == -1)
-          step.pos = new Position((float)numStep*2f, (float)PosY);
-        numStep+=1;
-      }
-    }
-    System.out.println("");
-
-    stepLL = Trans.target;  PosY+=1;  numStep=0;
-    System.out.println("  " + "TargetSteps:");
-    if (stepLL != null) {
-      for (i=0; i < stepLL.size(); i++) {
-        step = (Step)stepLL.get(i);
-        System.out.print("    " + "Step " + step.name + " : PosY=" + PosY + " - ");
-        if (step.pos == null || step.pos.x == -1)
-          step.pos = new Position(numStep, PosY);
-        numStep+=1;
-      }
-    }
-    System.out.println("");
-  }
-
-  public void realignSFC() {
-    Transition Trans;
-    LinkedList transLL = sfc.transs;
-    int PosY = 0, TransNum = 1, i= 0;
-
-    if (transLL != null) {
-      for (i = 0; i < transLL.size();  i++) {
-        Trans = (Transition)transLL.get(i);
-        System.out.println("Transition-Nr.: " + TransNum);
-        proccessTrans(Trans, PosY);
-        PosY+=3;  TransNum+=1;
-      }
-    }
-
-  }
-
-  public void repaintSFC() {
-    realignSFC();
-    repaint();
-  }
-
-  /**
-   * baut ein Editor-Fenster (momentan JFrame-Ableitung) auf.
-   * Übergeben werden muss ein SFC<>null
-   */
-  public Editor(SFC anSFC) throws EditorException {
-    setFilename("NoName");
-    setSize(600, 420);
-    setSFC(anSFC);
-
-    // ToolBar:
-    ToolBar = new JToolBar();
-    ToolBar.add(new ActInsertStep());
-    ToolBar.add(new ActInsertTrans());
-    ToolBar.add(new ActDeleteAbsynt());
+  /** 
+   * Konstante zum Highlight ausschalten 
+   * @see #highlight_state(Absynt Element, boolean Value) 
+   */ 
+  final public boolean HILIGHT_OFF = false; 
+  /** 
+   * Konstante zum Highlight einschalten 
+   * @see #highlight_state(Absynt Element, boolean Value) 
+  */ 
+  final public boolean HILIGHT_ON = true; 
+ 
+  /** 
+   * setzt den Dateinamen 
+   * (nur zur Anzeige in der Titelleise, Datei-IO macht ansonsten die GUI-Gruppe) 
+   */ 
+  private void setFilename(String Filename) { 
+    strName = new String(Filename); 
+    setTitle("Editor - " + strName); 
+    boolModified = false; 
+  } 
+ 
+  private void setSFC(SFC anSFC) throws EditorException { 
+    if (anSFC != null)  
+	sfc = anSFC; 
+    else        
+      throw new EditorException("Anwendungsfehler: SFC = null"); 
+  } 
+ 
+  private void proccessTrans(Transition Trans, int PosY) { 
+    Step step; 
+    LinkedList stepLL = Trans.source; 
+    int numStep = 0, i = 0; 
+ 
+    System.out.println("  " + "SourceSteps:"); 
+    if (stepLL != null) { 
+      for (i=0; i < stepLL.size(); i++) { 
+        step = (Step)stepLL.get(i); 
+        System.out.print("    " + "Step " + step.name + " : PosY=" + PosY + " - "); 
+        if (step.pos == null || step.pos.x == -1) 
+          step.pos = new Position((float)numStep*2f, (float)PosY); 
+        numStep+=1; 
+      } 
+    } 
+    System.out.println(""); 
+ 
+    stepLL = Trans.target;  PosY+=1;  numStep=0; 
+    System.out.println("  " + "TargetSteps:"); 
+    if (stepLL != null) { 
+      for (i=0; i < stepLL.size(); i++) { 
+        step = (Step)stepLL.get(i); 
+        System.out.print("    " + "Step " + step.name + " : PosY=" + PosY + " - "); 
+        if (step.pos == null || step.pos.x == -1) 
+          step.pos = new Position(numStep, PosY); 
+        numStep+=1; 
+      } 
+    } 
+    System.out.println(""); 
+  } 
+ 
+  public void realignSFC() { 
+    Transition Trans; 
+    LinkedList transLL = sfc.transs; 
+    int PosY = 0, TransNum = 1, i= 0; 
+ 
+    if (transLL != null) { 
+      for (i = 0; i < transLL.size();  i++) { 
+        Trans = (Transition)transLL.get(i); 
+        System.out.println("Transition-Nr.: " + TransNum); 
+        proccessTrans(Trans, PosY); 
+        PosY+=3;  TransNum+=1; 
+      } 
+    } 
+ 
+  } 
+ 
+  public void repaintSFC() { 
+    realignSFC(); 
+    repaint(); 
+  } 
+ 
+  /** 
+   * baut ein Editor-Fenster (momentan JFrame-Ableitung) auf. 
+   * Übergeben werden muss ein SFC<>null 
+   */ 
+  public Editor(SFC anSFC) throws EditorException { 
+    setFilename("NoName"); 
+    setSize(600, 420); 
+    setSFC(anSFC); 
+ 
+    // ToolBar: 
+    ToolBar = new JToolBar();     
+    ButtonGroup BtnGroup = new ButtonGroup();
+    SelectToggleBtn = new JToggleButton(new ActSelect());
+    BtnGroup.add(SelectToggleBtn);
+    BtnGroup.add(new JToggleButton(new ActInsertStep()));
+    BtnGroup.add(new JToggleButton(new ActInsertTrans()));
+    BtnGroup.add(new JToggleButton(new ActDeleteAbsynt()));    
     
-    ToolBar.setSize(400, 80);
-    getContentPane().add(ToolBar, BorderLayout.NORTH);    
-
-    // Panel für Variablen- / Aktionen-Deklaration:
-    DataPanel = new JPanel();
-    DataPanel.setSize(200, 0);
-    DataPanel.setLayout(new GridLayout(0, 1));
-    DataDeclPanel = new JPanel();
-    DataDeclPanel.setLayout(new BorderLayout());
-    DataDeclPanel.add(new JLabel("Deklarationen:"), BorderLayout.NORTH);
-    DataDeclTable = new JTable(1, 3);
-    // DataDeclTable.setTableHeader(new JTableHeader());
-    DataDeclPanel.add(DataDeclTable, BorderLayout.CENTER);
-    DataDeclPanel.add(new JButton("Neu"), BorderLayout.SOUTH);
-    DataDeclPanel.add(new JButton("Entf"), BorderLayout.SOUTH);
-    DataPanel.add(DataDeclPanel);
-    DataActPanel = new JPanel();
-    DataActPanel.setLayout(new BorderLayout());
-    DataActPanel.add(new JLabel("Aktionen:"), BorderLayout.NORTH);
-    DataActTable = new JTable(1, 2);
-    // DataActTable.setTableHeader(new JTableHeader());
-    DataActPanel.add(DataActTable, BorderLayout.CENTER);
-    DataActPanel.add(new JButton("Neu"), BorderLayout.SOUTH);
-    DataActPanel.add(new JButton("Entf"), BorderLayout.SOUTH);
-    DataPanel.add(DataActPanel);
+    Enumeration Enum = BtnGroup.getElements();
+    while (Enum.hasMoreElements()) 
+      ToolBar.add((JToggleButton)Enum.nextElement()); 
+      
+      /*
+    ToolBar.add(new JToggleButton(new ActInsertTrans())); 
+    ToolBar.add(new JToggleButton(new ActDeleteAbsynt())); 
+      */
+    ToolBar.setSize(400, 80); 
+    getContentPane().add(ToolBar, BorderLayout.NORTH);     
     
-    // DataPanel.add(new JTable, BorderLayout.NORTH);
-    DataScrPane = new JScrollPane(DataPanel);
-    DataScrPane.setSize(200, 0);
-    getContentPane().add(DataScrPane, BorderLayout.WEST);
-
-    // Zeichenfläche für SFC:
-    DrawPanel = new DrawSFCPanel();
-    DrawPanel.setBackground(Color.white);
-
-    DrawScrPane = new JScrollPane(DrawPanel);
-    getContentPane().add(DrawScrPane, BorderLayout.CENTER);
-
-    repaintSFC();
-
-  }
-
-  /**
-   * setzt den Highlight-Status eines Absynt-Elements -
-   * @see #HILIGHT_ON
-   * @see #HILIGHT_OFF
-   */
-  public void highlight_state(Absynt Element, boolean Value) {
-  }
-
-  private void setSelected(Absynt Element, boolean Value) {
-  }
-
-  /**
-   * gibt an, ob der SFC seit dem letzten Speichern/Laden geändert wurde
-   */
-  public boolean isModified() {
-    return(boolModified);
-  }
-
-  private void setModified(boolean Value) {
-    boolModified = Value;
-    if (boolModified) boolChecked = false;
-  }
-
-  /**
-   * gibt an, ob der SFC gecheckt ist
-   * @see #setChecked()
-   */
-  public boolean isChecked() {
-    return(boolChecked);
-  }
-
-  /**
-   * teilt dem Editor mit, dass sein SFC gecheckt ist - sobald der SFC im Editor
-   * geändert wird, ist der SFC für den Editor nicht mehr gecheckt
-   * @see #isModified()
-   */
-  public void setChecked() {
-    boolChecked =  true;
-  }
-
-}
+ 
+    // Panel für Variablen- / Aktionen-Deklaration: 
+    DataPanel = new JPanel(); 
+    DataPanel.setSize(200, 0); 
+    DataPanel.setLayout(new GridLayout(0, 1)); 
+    DataDeclPanel = new JPanel(); 
+    DataDeclPanel.setLayout(new BorderLayout()); 
+    DataDeclPanel.add(new JLabel("Deklarationen:"), BorderLayout.NORTH); 
+    String[] Header = {"Name", "Typ", "Wert"};
+    String[][] Values = {{"x", "bool", "false"}};
+    DataDeclTable = new JTable(Values, Header); 
+    // DataDeclTable.setTableHeader(new JTableHeader()); 
+    DataDeclPanel.add(new JScrollPane(DataDeclTable), BorderLayout.CENTER); 
+    DataDeclPanel.add(new JButton("Neu"), BorderLayout.SOUTH); 
+    DataDeclPanel.add(new JButton("Entf"), BorderLayout.SOUTH); 
+    DataPanel.add(DataDeclPanel); 
+    DataActPanel = new JPanel(); 
+    DataActPanel.setLayout(new BorderLayout()); 
+    DataActPanel.add(new JLabel("Aktionen:"), BorderLayout.NORTH); 
+    DataActTable = new JTable(1, 2); 
+    // DataActTable.setTableHeader(new JTableHeader()); 
+    DataActPanel.add(DataActTable, BorderLayout.CENTER); 
+    DataActPanel.add(new JButton("Neu"), BorderLayout.SOUTH); 
+    DataActPanel.add(new JButton("Entf"), BorderLayout.SOUTH); 
+    DataPanel.add(DataActPanel); 
+     
+    // DataPanel.add(new JTable, BorderLayout.NORTH); 
+    DataScrPane = new JScrollPane(DataPanel); 
+    DataScrPane.setSize(200, 0); 
+    getContentPane().add(DataScrPane, BorderLayout.WEST); 
+ 
+    // Zeichenfläche für SFC: 
+    DrawPanel = new DrawSFCPanel(sfc); 
+    DrawPanel.setBackground(Color.white); 
+ 
+    DrawScrPane = new JScrollPane(DrawPanel); 
+    getContentPane().add(DrawScrPane, BorderLayout.CENTER); 
+ 
+    repaintSFC(); 
+ 
+  } 
+ 
+  /** 
+   * setzt den Highlight-Status eines Absynt-Elements - 
+   * @see #HILIGHT_ON 
+   * @see #HILIGHT_OFF 
+   */ 
+  public void highlight_state(Absynt Element, boolean Value) { 
+  } 
+ 
+  private void setSelected(Absynt Element, boolean Value) { 
+  } 
+ 
+  /** 
+   * gibt an, ob der SFC seit dem letzten Speichern/Laden geändert wurde 
+   */ 
+  public boolean isModified() { 
+    return(boolModified); 
+  } 
+ 
+  private void setModified(boolean Value) { 
+    boolModified = Value; 
+    if (boolModified) boolChecked = false; 
+  } 
+ 
+  /** 
+   * gibt an, ob der SFC gecheckt ist 
+   * @see #setChecked() 
+   */ 
+  public boolean isChecked() { 
+    return(boolChecked); 
+  } 
+ 
+  /** 
+   * teilt dem Editor mit, dass sein SFC gecheckt ist - sobald der SFC im Editor 
+   * geändert wird, ist der SFC für den Editor nicht mehr gecheckt 
+   * @see #isModified() 
+   */ 
+  public void setChecked() { 
+    boolChecked =  true; 
+  } 
+ 
+} 
