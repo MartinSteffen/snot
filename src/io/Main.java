e3de4c03982a0f98d8f320f45c3966d8a62f7164
swprakt <msteffen@ifi.uio.no>
2001-07-10 14:28:06 +0000

    OK

--- src/io/Main.java
+++ src/io/Main.java
@@ -4,12 +4,14 @@ package io;
 
 import java.io.IOException;
 import java.io.*;
+import utils.*;
+import absynt.*;
 
 
 /**
  * Standalone file to test the parser on input files
  * @author Martin Steffen, Karsten Stahl
- * @version $Id: Main.java,v 1.3 2001-06-30 15:03:22 swprakt Exp $
+ * @version $Id: Main.java,v 1.4 2001-07-10 14:28:06 swprakt Exp $
  *
  * ============== //-->
  */
@@ -23,7 +25,10 @@ public class Main {
       File datei = new File(args[0]);
       
       io.Parser parser = new io.Parser();
-      parser.parseFile(datei);
+      PrettyPrint pp = new PrettyPrint();
+      SFC sfc1 =  parser.parseFile(datei);
+      System.out.println("*** Test: pretty print the result of parsing ***");
+      pp.print(sfc1);      
     } catch (ParseException e){
       System.err.println(e.toString());
       System.exit(1);
